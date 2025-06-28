e725d77ccb691a05163ca1ebe56deae9ef31b757
[dyad] <git@dyad.sh>
2025-06-28 18:12:02 -0400

    [dyad] Updated desktop background and icon text color. - wrote 2 file(s) + extra files edited outside of Dyad

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -14,7 +14,7 @@ const DesktopIcon: React.FC<DesktopIconProps> = ({ iconSrc, label, onClick, clas
       onClick={onClick}
       className={cn(
         "flex flex-col items-center justify-center p-3 w-24 h-24",
-        "text-mac-black text-xs font-sans",
+        "text-mac-white text-xs font-sans",
         "hover:bg-mac-medium-gray/30 active:bg-mac-medium-gray/50 rounded-sm",
         "focus:outline-none focus:bg-mac-medium-gray/50"
       )}
